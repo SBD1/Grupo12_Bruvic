2ba2e21999e1a4f6306ab7a72666a38d67ede24d
Shayane <shayaneliebe@gmail.com>
2022-04-16 22:02:51 -0300

    Add more trigger statement

--- Modulo 4 - SQL/src/scripts/triggers/items_triggers.sql
+++ Modulo 4 - SQL/src/scripts/triggers/items_triggers.sql
@@ -15,6 +15,33 @@ BEGIN
 END;
 $cria_venda$ LANGUAGE plpgsql;
 DROP TRIGGER cria_venda ON Item;
+
 CREATE TRIGGER cria_venda
 AFTER UPDATE ON Item
 FOR EACH ROW EXECUTE PROCEDURE cria_venda();
+
+
+-- triger de statement no momento da deleção de um item
+CREATE OR REPLACE FUNCTION statement_remove_item() RETURNS trigger AS $statement_remove_item$
+BEGIN
+    RAISE NOTICE 'Item removido da mochila, ele não poderá ser resgatado';
+    RETURN OLD;
+END;
+$statement_remove_item$ LANGUAGE plpgsql;
+
+CREATE TRIGGER statement_remove_item
+BEFORE DELETE ON ITEM
+FOR EACH ROW EXECUTE PROCEDURE statement_remove_item();
+
+
+-- trigger do statement no momento de inserir ou atualizar um item em uma mochila
+CREATE OR REPLACE FUNCTION statement_insert_item() RETURNS trigger AS $statement_insert_item$
+BEGIN
+    RAISE NOTICE 'Um item foi inserido na mochila.';
+    RETURN NEW;
+END;
+$statement_insert_item$ LANGUAGE plpgsql;
+
+CREATE TRIGGER statement_insert_item
+BEFORE INSERT OR UPDATE ON ITEM
+FOR EACH ROW EXECUTE PROCEDURE statement_insert_item();
