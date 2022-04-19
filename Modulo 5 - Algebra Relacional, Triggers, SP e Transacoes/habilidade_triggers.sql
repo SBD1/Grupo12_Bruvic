f6fc07d05716eb35b4aa5c8602385a3343699dc7
gabrieldvpereira <gdavi.gd@gmail.com>
2022-04-18 21:54:14 -0300

    add check trigger for cure

--- Modulo 5 - Algebra Relacional, Triggers, SP e Transacoes/habilidade_triggers.sql
+++ Modulo 5 - Algebra Relacional, Triggers, SP e Transacoes/habilidade_triggers.sql
@@ -53,3 +53,41 @@ CREATE TRIGGER cria_ataque_trigger
 BEFORE INSERT ON Ataque
 FOR EACH ROW EXECUTE PROCEDURE cria_ataque();
 
+-- ======================================= CHECK CURA  ===========================================================================================
+
+CREATE OR REPLACE FUNCTION cria_cura() RETURNS trigger AS $cria_cura$
+DECLARE 
+    ataques INTEGER;
+    truques INTEGER;
+    magias INTEGER; 
+BEGIN 
+    ataques := COUNT(*) FROM Ataque where habilidade = new.habilidade;
+    IF ataques > 0 THEN 
+        RAISE EXCEPTION 'Já existe um ataque com essa referência de habilidade!';
+        RETURN NULL;
+    END IF; 
+
+    truques := COUNT(*) FROM Truque WHERE habilidade = new.habilidade; 
+    IF truques > 0 THEN 
+        RAISE EXCEPTION 'Já existe um truque com essa referência de habilidade!';
+        RETURN NULL;
+    END IF; 
+    
+    magias := COUNT(*) FROM Magia WHERE habilidade = new.habilidade; 
+    IF magias > 0 THEN
+        RAISE EXCEPTION 'Já existe uma magia com essa referência de habilidade!'; 
+        RETURN NULL;
+    END IF;
+
+    UPDATE Habilidade_Tipo 
+    SET tipo = 'Cura'
+    WHERE nome = new.habilidade;
+
+    return new; 
+END;
+
+$cria_cura$ LANGUAGE plpgsql;
+
+CREATE TRIGGER cria_cura_trigger
+BEFORE INSERT ON Cura
+FOR EACH ROW EXECUTE PROCEDURE cria_cura();
