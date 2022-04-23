672afd1858f9bd0e70bf287e40797d235da19bbf
Shayane <shayaneliebe@gmail.com>
2022-04-23 19:37:54 -0300

    Add get personagem in db

--- game/src/models/personagens/personagens_manager.js
+++ game/src/models/personagens/personagens_manager.js
@@ -1,5 +1,5 @@
 const db = require("../../db/db_config");
-const Negociante = require("./negociante");
+const Personagem = require("./personagem");
 
 module.exports = class PersonagensManager {
 
@@ -7,23 +7,17 @@ module.exports = class PersonagensManager {
         const client = await db.connect();
         const querySelectByName = `SELECT DISTINCT * from DefinicaoPersonagem def LEFT JOIN Personagem personagem  ON personagem.id = def.personagem WHERE personagem.nome = '${nome}';`
 
-        const retrived_negociantes = await client.query(querySelectByName).then((res)=>{
-            const negociantes = [];
-            res.rows.forEach((negociante) => {
-                negociantes.push(new Negociante(
-                    negociante.id,
-                    negociante.eixo_x,
-                    negociante.eixo_y,
-                    negociante.mapa,
-                    negociante.nome,
-                    negociante.tipo,
-                    negociante.vida,
-                    negociante.montante
-                ));
-            });
-            return negociantes;
+        const retrived_personagem = await client.query(querySelectByName).then((res)=>{
+            const personagem_info = res.rows[0];
+            if (personagem_info){
+                const personagem = new Personagem(personagem_info.id, personagem_info.vida, personagem_info.nome, 
+                    personagem_info.destreza,personagem_info.forca, personagem_info.constituicao,
+                    personagem_info.carisma, personagem_info.sabedoria, personagem_info.inteligencia, personagem_info.montante,
+                    personagem_info.raca, personagem_info.classe, personagem_info.classe_de_armadura, personagem_info.experiencia, personagem_info.nivel)
+                return personagem;
+            }
+            return false;
         });
-        return await retrived_negociantes;
+        return await retrived_personagem;
     }
-
 }
