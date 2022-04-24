8e1642ac8b4df8b6ea0b974759b316c7bcd0b6e8
Shayane <shayaneliebe@gmail.com>
2022-04-23 22:33:31 -0300

    Create logic for showing items to user

--- game/src/user_interaction/praca_negociantes/praca_negociantes_control.js
+++ game/src/user_interaction/praca_negociantes/praca_negociantes_control.js
@@ -8,5 +8,16 @@ module.exports = {
 
         const mensagem_praca = await forms.choiceFromNegocianteList(negociantes);
         console.log(mensagem_praca);
+        if(mensagem_praca.selecaoNegociante == forms.BACK_MENU){
+            //TODO: something
+            console.log("voltar ao menu anterior")
+        } else {
+            console.log("mostra os itens")
+            const negocianteSelecionado = negociantes.filter(negociante => {
+                return negociante.nome === mensagem_praca.selecaoNegociante
+            })[0];
+            const itemsNegociante = await ItemsManager.getAllFromNegociante(negocianteSelecionado).then((res) => {return res});
+            forms.choiceFromItemsList(negocianteSelecionado, itemsNegociante);
+        }
     }
 }
--- game/src/user_interaction/praca_negociantes/praca_negociantes_forms.js
+++ game/src/user_interaction/praca_negociantes/praca_negociantes_forms.js
@@ -38,8 +38,52 @@ async function choiceFromNegocianteList(negociantes) {
   return inquirer.prompt(questions);
 }
 
+
+async function choiceFromItemList(negociante, items) {
+  console.log(
+    chalk.green(
+        `Essa é a loja de ${negociante.nome}. Ele dá as boas-vindas para todos os pechinchadores.`
+    )
+    );
+
+    const clui = require('clui'),
+     clc = require('cli-color'),
+    Line = clui.Line;
+
+    var headers = new Line()
+      .padding(2)
+      .column('Nome', 30, [clc.cyan])
+      .column('Preço', 10, [clc.cyan])
+      .column('Peso', 10, [clc.cyan])
+      .fill();
+
+  const items_choices = items.map((item) => { 
+    item_name_line = new Line()
+    .padding(2)
+    .column(item.nome, 30, [clc.cyan])
+    .column(String(item.preco), 10, [clc.cyan])
+    .column(String(item.peso), 10, [clc.cyan])
+    .fill();
+    return {name: item_name_line.contents(), value: item, short: item.nome}
+  });
+  items_choices.push({name: BACK_MENU, value: BACK_MENU, short:BACK_MENU});
+
+  const questions = [
+  {
+    type: 'list',
+    name: 'selecaoItem',
+    message: `Veja a lista de itens disponíveis para compra\n ${headers.contents()}`,
+    choices: items_choices,
+    default: BACK_MENU
+  }
+  ];
+  return inquirer.prompt(questions);
+}
+
+
 module.exports = {
   choiceFromNegocianteList: choiceFromNegocianteList,
+  choiceFromItemsList: choiceFromItemList,
   BACK_MENU: BACK_MENU
 };
 
