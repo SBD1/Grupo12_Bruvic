75702871b61bf46b16b35ead1298f2ad222da1c4
Hugo Sobral <hugosobral10@gmail.com>
2022-04-27 19:39:01 -0300

    Create initial version of new character creation user interface

--- game/src/user_interaction/personagem/createCharacter.js
+++ game/src/user_interaction/personagem/createCharacter.js
@@ -1,12 +1,36 @@
 const clear = require('clear');
-const rl = require('readline');
-
-readline = rl.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const inquirer = require('inquirer');
 
 var characterName;
+var characterRace;
+
+const nameQuestionId = 'nameOpt';
+const nameQuestion = [
+    {
+        name: nameQuestionId,
+        type: 'input',
+        message: 'Digite o nome do seu personagem: ',
+        validate: async function( value ) {
+            const newVal = value.trim();
+            const check = newVal.length;
+            return check<1 ? 'O nome não pode ser vazio' : true; 
+        }
+    }
+];
+
+const raceQuestionId = 'raceOpt';
+const raceQuestion = [
+    {
+        name: raceQuestionId,
+        type: 'input',
+        message: 'Escolha a raça do seu personagem: ',
+        validate: async function (value) {
+            const check = parseInt(value);
+            return (isNaN(check) || check<1 || check>3) ?
+            'A opção precisa ser um número entre 1 e 3' : true;
+        }
+    }
+];
 
 const buildTitle = () => {
     clear();
@@ -26,19 +50,54 @@ const buildTitle = () => {
     console.log();
 };
 
-const getCharacterName = () => {
-    readline.question("Qual vai ser o nome do personagem:  ", function(answer) {
-        characterName = answer;
-        console.log(characterName);
-    });
+const capitalize = (str) => (str.charAt(0).toUpperCase() + str.slice(1));
+
+const getCharacterName = async () => {
+    const answer = await inquirer.prompt(nameQuestion);
+    characterName = capitalize(answer[nameQuestionId].trim());
 };
+
+const chooseRace = () => {
+    inquirer.prompt(raceQuestion).then((answer) => {
+        const opt = parseInt(answer[raceQuestionId]);
+        var race;
+        if (opt == 1) {
+            race = 'Guerreiro';
+        } else if (opt == 2) {
+            race = 'Mago';
+        } else {
+            race = 'Clerigo';
+        }
+
+        characterRace = race;
+        console.log(characterRace);
+    });
+}
+
+const getRace = () => {
+    // clear();
+    console.log();
+    console.log();
+    console.log("------ Selecione sua raça -------");
+    console.log('1) Guerreiro');
+    console.log('2) Mago');
+    console.log('3) Clérigo');
+    console.log();
+    chooseRace();
+};
+
+const getCharClass = () => {
+
+};
+
+const getAttributes = () => {};
 
 const createNewCharacter = async () => {
     buildTitle();
     console.log("----- Criação de personagem -----");
     console.log();
-    await getCharacterName();
-    console.log('Oh no');
+    getCharacterName();
+    getRace();
 };
 
 module.exports = createNewCharacter;
