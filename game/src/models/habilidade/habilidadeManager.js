7f7da4d3af6ac36180f50bef9f78d82d714a4bb4
Gabriel Davi <gdavi.gd@gmail.com>
2022-04-26 22:37:12 -0300

    ceate habilidades manager

--- game/src/models/habilidade/habilidadeManager.js
+++ game/src/models/habilidade/habilidadeManager.js
@@ -1,10 +1,35 @@
+const Habilidade = require('./habilidade'); 
+const db = require("../../db/db_config");
+
 module.exports = class HabilidadeManager {
-    static async get(nome){
+    static async getByName(nome){
       const client = await db.connect();
-      const querySelectByName = `SELECT * from HABILIDADE where nome = '${nome}'`
+      const querySelectByName = `SELECT * from HABILIDADE JOIN HABILIDADE_TIPO ON HABILIDADE.nome = HABILIDADE_TIPO.nome WHERE HABILIDADE.nome = '${nome}'`
 
       const query = await client.query(querySelectByName);
-      console.log(query);
+      const habilidade = query.rows[0]; 
       client.release();
+
+      if(habilidade){
+          return new Habilidade(
+            habilidade.nome, 
+            habilidade.descricao,
+            habilidade.nivel, 
+            habilidade.tipo
+          ); 
+      }
+  }
+
+  static async getAllByTipo(tipo){
+    const client = await db.connect();
+    const querySelectByName = `SELECT * from HABILIDADE JOIN HABILIDADE_TIPO ON HABILIDADE.nome = HABILIDADE_TIPO.nome WHERE HABILIDADE_TIPO.tipo = '${tipo}'`
+    const query = await client.query(querySelectByName);
+    client.release();
+    return query.rows.map(habilidade => new Habilidade(
+      habilidade.nome, 
+      habilidade.descricao,
+      habilidade.nivel, 
+      habilidade.tipo
+    ));
   }
 }
