09669ce229785f992fa3cae06f8033d3271eb905
gabrieldvpereira <gdavi.gd@gmail.com>
2022-04-27 23:36:17 -0300

    add ability to add habilidade and get habilidade from a user

--- game/src/models/habilidade/habilidade_manager.js
+++ game/src/models/habilidade/habilidade_manager.js
@@ -4,8 +4,7 @@ const db = require("../../db/db_config");
 module.exports = class HabilidadeManager {
     static async getByName(nome){
       const client = await db.connect();
-      const querySelectByName = `SELECT * from HABILIDADE JOIN HABILIDADE_TIPO ON HABILIDADE.nome = HABILIDADE_TIPO.nome WHERE HABILIDADE.nome = '${nome}'`
-
+      const querySelectByName = `SELECT * from HABILIDADE JOIN HABILIDADE_TIPO ON HABILIDADE.nome = HABILIDADE_TIPO.nome WHERE HABILIDADE.nome = '${nome}';`
       const query = await client.query(querySelectByName);
       const habilidade = query.rows[0]; 
       client.release();
@@ -22,7 +21,7 @@ module.exports = class HabilidadeManager {
 
   static async getAllByTipo(tipo){
     const client = await db.connect();
-    const querySelectByName = `SELECT * from HABILIDADE JOIN HABILIDADE_TIPO ON HABILIDADE.nome = HABILIDADE_TIPO.nome WHERE HABILIDADE_TIPO.tipo = '${tipo}'`
+    const querySelectByName = `SELECT * from HABILIDADE JOIN HABILIDADE_TIPO ON HABILIDADE.nome = HABILIDADE_TIPO.nome WHERE HABILIDADE_TIPO.tipo = '${tipo}';`
     const query = await client.query(querySelectByName);
     client.release();
     return query.rows.map(habilidade => new Habilidade(
--- game/src/models/personagens/personagens_manager.js
+++ game/src/models/personagens/personagens_manager.js
@@ -1,10 +1,9 @@
 const db = require("../../db/db_config");
 const Personagem = require("./personagem");
-
+const HabilidadeManager = require('../habilidade/habilidade_manager'); 
 module.exports = class PersonagensManager {
 
     static async getPersonagemAtual(nome="bolinha"){
-
         const client = await db.connect();
         const querySelectByName = `SELECT DISTINCT * from DefinicaoPersonagem def LEFT JOIN Personagem personagem  ON personagem.id = def.personagem WHERE personagem.nome = '${nome}';`
 
@@ -33,10 +32,65 @@ module.exports = class PersonagensManager {
             personagem_info.experiencia, 
             personagem_info.nivel
         );
+
         const mochila = await client.query(`SELECT * FROM MOCHILA WHERE personagem = ${personagem_info.id}`)
         const mochila_info = mochila.rows[0]; 
-        personagem.setMochilaInfo(mochila_info.capacidade, mochila_info.id);
+        if(mochila_info){
+            personagem.setMochilaInfo(mochila_info.capacidade, mochila_info.id);
+        }
+        
+
+        if(personagem.classe === 'Clerigo'){
+            const cura = await client.query(`SELECT * FROM ClerigoCura WHERE clerigo = ${personagem.id}`);
+            const cura_info_promisses =  cura.rows.map(cura_info => HabilidadeManager.getByName(cura_info.cura)); 
+            const cura_info = await Promise.all(cura_info_promisses); 
+            if(cura_info && cura_info.length > 0){
+                personagem.habilidades = [...personagem.habilidades, ...cura_info]
+            }
+
+            const truque = await client.query(`SELECT * FROM ClerigoTruque WHERE clerigo = ${personagem.id}`);
+            const truque_info_promisses =  truque.rows.map(truque_info => HabilidadeManager.getByName(truque_info.truque)); 
+            const truque_info = await Promise.all(truque_info_promisses); 
+            if(truque_info && truque_info.length > 0){
+                personagem.habilidades = [...personagem.habilidades, ...truque_info]
+            }
+        }
+        
+        if(personagem.classe  === 'Mago'){
+            const magia = await client.query(`SELECT * FROM MagoMagia WHERE mago = ${personagem.id}`);
+            const magia_info_promisses =  magia.rows.map(magia_info => HabilidadeManager.getByName(magia_info.magia)); 
+            const magia_info = await Promise.all(magia_info_promisses); 
+            if(magia_info && magia_info.length > 0){
+                personagem.habilidades = [...personagem.habilidades, ...magia_info]
+            }
+        }
+
+        
         client.release();
         return personagem; 
+    }
+
+    static async addHabiliadesToPersonagem(personagem, habilidade){
+        const client = await db.connect();
+    
+        if(!personagem.classe || !habilidade.tipo) return; 
+
+        let query; 
+        if(personagem.classe  === 'Clerigo' && habilidade.tipo === 'Cura'){
+            query = 'ClerigoCura(clerigo, cura)'; 
+        }
+
+        if(personagem.classe  === 'Clerigo' && habilidade.tipo === 'Truque'){
+            query = 'ClerigoTruque(clerigo, truque)'; 
+        }
+
+        if(personagem.classe  === 'Mago' && habilidade.tipo === 'Magia'){
+            query = 'MagoMagia(mago, magia)'; 
+        }
+       const result =  await client.query(`INSERT INTO ${query} VALUES (${personagem.id}, '${habilidade.nome}');`)
+       console.log(result);
+       client.release();
+       personagem.setNewHabilidade(habilidade); 
+       return personagem;
     }
 }
