e80ad94b76e5a3040a994174743e446e6a99c248
Hugo Sobral <hugosobral10@gmail.com>
2022-04-29 18:09:45 -0300

    Tell story game

--- game/src/user_interaction/menu/mainMenu.js
+++ game/src/user_interaction/menu/mainMenu.js
@@ -9,7 +9,6 @@ const navigation = require("../mapa/renderMapa");
 const PersonagensManager = require('../../models/personagens/personagens_manager');
 
 const optionQuestionId = "mainMenuOpt";
-
 const optionQuestion = [
   {
     name: optionQuestionId,
@@ -24,9 +23,46 @@ const optionQuestion = [
   },
 ];
 
+const storyQuestionId = "storyOpt";
+const storyQuestion = [
+  {
+    name: storyQuestionId,
+    type: "input",
+    message: "Digite qualquer tecla para continuar...",
+    validate: function (value) {
+      return true;
+    },
+  },
+];
+
 const cleanScreen = () => {
   clear();
   console.log();
+};
+
+const tellStoryGame = async () => {
+  cleanScreen();
+  console.log("Bruvic é uma cidade dentro do continente de Bely-Krugg. Bely-Krugg é um ambiente extenso, na sua maioria composto por planícies");
+  console.log("nevadas. O continente de Bely-krugg tem três grandes povos: os Filhos de Thok, os Sábios de Kou e os Seguidores de Brum, as três");
+  console.log("grandes criaturas celestiais que criaram Bely-Krugg. Historicamente, as três entidades nunca tiveram uma relação pacífica ou ");
+  console.log("amistosa, e assim a fizeram as três grandes tribos do continente.");
+  console.log();
+  console.log("Bruvic é uma cidade com uma temperatura mais elevada, ambiente raro em Bely-Krugg, e não está no território de nenhuma das três tribos.");
+  console.log("Assim como a sua natureza política, as orientações culturais e religiosas da cidade são bastante variadas.")
+  console.log();
+  console.log("Bruvic é uma cidade portuária: é possível encontrar grandes portos, navios de todo o canto do mundo e as mais diversas mercadorias de todo o continente. Em sua");
+  console.log("maioria, os cenários são compostos por grandes construções de madeira embasadas em estruturas de pier, que por sua vez também são construídas de madeira. Apesar");
+  console.log("do cenário que remete bastante a portos piratas, a cidade apresenta pouca ou nenhuma hostilidade para com visitantes.")
+  console.log("Pela cidade não possuir estratificação e uma notável neutralidade, em Bely-Krugg isso é uma característica ainda mais rara do que ser uma cidade quente, os povos do continente se reúnem");
+  console.log("anualmente para celebrar um breve momento de paz política e militar e ainda realizar confraternizações religiosas. Tal evento é conhecido como a Procissão.");
+  console.log();
+  console.log("Atualmente você está na taverna em que você se hospedou. Você se encontra num bar com várias pessoas.");
+  console.log();
+  console.log("Você é mais um habitante de Bely-Krugg que se dirigiu a Bruvic para participar da Procissão. E tudo estava ocorrendo bem, até que...");
+  console.log();
+  console.log();
+  console.log();
+  await inquirer.prompt(storyQuestion);
 };
 
 const logTab = (line=1) => {
@@ -51,12 +87,10 @@ const handleInput = async (inp) => {
     const newPersonagem = await createNewCharacter();
     const personagemWithHabilidade = await addHabilidadeToPersonagem(newPersonagem); 
     console.log(personagemWithHabilidade);
-    // cleanScreen();
+    cleanScreen();
     const personagemLocalizacao = await PersonagensManager.getPersonagemLocalizacao(newPersonagem);
-    console.log("AAAA");
     console.log(personagemLocalizacao);
-    console.log("LÁ VAMOS NÓS GRANDE AVENTUREIRO");
-    console.log("aqui explicação da história");
+    await tellStoryGame();
 
     navigation();
   }
