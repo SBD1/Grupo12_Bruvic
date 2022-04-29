b6739242e703a0f39fa1a5121fb3d6e89c462316
gabrieldvpereira <gdavi.gd@gmail.com>
2022-04-29 09:10:41 -0300

    change init file

---
+++ game/src/inital.js
@@ -0,0 +1,19 @@
+const clear = require('clear');
+const buildTitle = () => {
+    clear();
+    console.log();
+    console.log();
+    console.log("▀█████████▄     ▄████████ ███    █▄   ▄█    █▄   ▄█   ▄████████");
+    console.log("  ███    ███   ███    ███ ███    ███ ███    ███ ███  ███    ███");
+    console.log("  ███    ███   ███    ███ ███    ███ ███    ███ ███▌ ███    █▀");
+    console.log(" ▄███▄▄▄██▀   ▄███▄▄▄▄██▀ ███    ███ ███    ███ ███▌ ███ ");
+    console.log("▀▀███▀▀▀██▄  ▀▀███▀▀▀▀▀   ███    ███ ███    ███ ███▌ ███ ");
+    console.log("  ███    ██▄ ▀███████████ ███    ███ ███    ███ ███  ███    █▄");
+    console.log("  ███    ███   ███    ███ ███    ███ ███    ███ ███  ███    ███");
+    console.log("▄█████████▀    ███    ███ ████████▀   ▀██████▀  █▀   ████████▀")
+    console.log("               ███    ███");   
+    console.log();
+    console.log();
+    console.log();
+};
+buildTitle();
