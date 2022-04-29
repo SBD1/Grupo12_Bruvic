28b3f3733f0af2273d07866368242a628fa262eb
gabrieldvpereira <gdavi.gd@gmail.com>
2022-04-29 09:18:14 -0300

    add start message to the init page of the game

--- game/src/inital.js
+++ game/src/inital.js
@@ -15,5 +15,6 @@ const buildTitle = () => {
     console.log();
     console.log();
     console.log();
+    console.log("Abra uma nova aba de seu terminal (sem fechar a abar atual) e digite 'make start-game' para iniciar o jogo");
 };
 buildTitle();
