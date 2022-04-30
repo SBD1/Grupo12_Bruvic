960eb456bfad98130acacee5609783850bc559b7
Leonardo Gomes <leonardodasigomes@gmail.com>
2022-04-30 12:13:54 -0300

    :bug: Remove then from function with return already

--- game/src/user_interaction/menu/mainMenu.js
+++ game/src/user_interaction/menu/mainMenu.js
@@ -37,49 +37,75 @@ const storyQuestion = [
 
 const tellStoryGame = async () => {
   cleanScreen();
-  console.log("Bruvic é uma cidade dentro do continente de Bely-Krugg. Bely-Krugg é um ambiente extenso, na sua maioria composto por planícies");
-  console.log("nevadas. O continente de Bely-krugg tem três grandes povos: os Filhos de Thok, os Sábios de Kou e os Seguidores de Brum, as três");
-  console.log("grandes criaturas celestiais que criaram Bely-Krugg. Historicamente, as três entidades nunca tiveram uma relação pacífica ou ");
-  console.log("amistosa, e assim a fizeram as três grandes tribos do continente.");
+  console.log(
+    "Bruvic é uma cidade dentro do continente de Bely-Krugg. Bely-Krugg é um ambiente extenso, na sua maioria composto por planícies"
+  );
+  console.log(
+    "nevadas. O continente de Bely-krugg tem três grandes povos: os Filhos de Thok, os Sábios de Kou e os Seguidores de Brum, as três"
+  );
+  console.log(
+    "grandes criaturas celestiais que criaram Bely-Krugg. Historicamente, as três entidades nunca tiveram uma relação pacífica ou "
+  );
+  console.log(
+    "amistosa, e assim a fizeram as três grandes tribos do continente."
+  );
   console.log();
-  console.log("Bruvic é uma cidade com uma temperatura mais elevada, ambiente raro em Bely-Krugg, e não está no território de nenhuma das três tribos.");
-  console.log("Assim como a sua natureza política, as orientações culturais e religiosas da cidade são bastante variadas.")
+  console.log(
+    "Bruvic é uma cidade com uma temperatura mais elevada, ambiente raro em Bely-Krugg, e não está no território de nenhuma das três tribos."
+  );
+  console.log(
+    "Assim como a sua natureza política, as orientações culturais e religiosas da cidade são bastante variadas."
+  );
   console.log();
-  console.log("Bruvic é uma cidade portuária: é possível encontrar grandes portos, navios de todo o canto do mundo e as mais diversas mercadorias de todo o continente. Em sua");
-  console.log("maioria, os cenários são compostos por grandes construções de madeira embasadas em estruturas de pier, que por sua vez também são construídas de madeira. Apesar");
-  console.log("do cenário que remete bastante a portos piratas, a cidade apresenta pouca ou nenhuma hostilidade para com visitantes.")
-  console.log("Pela cidade não possuir estratificação e uma notável neutralidade, em Bely-Krugg isso é uma característica ainda mais rara do que ser uma cidade quente, os povos do continente se reúnem");
-  console.log("anualmente para celebrar um breve momento de paz política e militar e ainda realizar confraternizações religiosas. Tal evento é conhecido como a Procissão.");
+  console.log(
+    "Bruvic é uma cidade portuária: é possível encontrar grandes portos, navios de todo o canto do mundo e as mais diversas mercadorias de todo o continente. Em sua"
+  );
+  console.log(
+    "maioria, os cenários são compostos por grandes construções de madeira embasadas em estruturas de pier, que por sua vez também são construídas de madeira. Apesar"
+  );
+  console.log(
+    "do cenário que remete bastante a portos piratas, a cidade apresenta pouca ou nenhuma hostilidade para com visitantes."
+  );
+  console.log(
+    "Pela cidade não possuir estratificação e uma notável neutralidade, em Bely-Krugg isso é uma característica ainda mais rara do que ser uma cidade quente, os povos do continente se reúnem"
+  );
+  console.log(
+    "anualmente para celebrar um breve momento de paz política e militar e ainda realizar confraternizações religiosas. Tal evento é conhecido como a Procissão."
+  );
   console.log();
-  console.log("Atualmente você está na taverna em que você se hospedou. Você se encontra num bar com várias pessoas.");
+  console.log(
+    "Atualmente você está na taverna em que você se hospedou. Você se encontra num bar com várias pessoas."
+  );
   console.log();
-  console.log("Você é mais um habitante de Bely-Krugg que se dirigiu a Bruvic para participar da Procissão. E tudo estava ocorrendo bem, até que...");
+  console.log(
+    "Você é mais um habitante de Bely-Krugg que se dirigiu a Bruvic para participar da Procissão. E tudo estava ocorrendo bem, até que..."
+  );
   console.log();
   console.log();
   console.log();
   await inquirer.prompt(storyQuestion);
 };
 
-const logTab = (line=1) => {
-    for (var i=0; i<line; i++) console.log();
+const logTab = (line = 1) => {
+  for (var i = 0; i < line; i++) console.log();
 };
 
-const NOVO_PERSONAGEM = 'Novo personagem';
-const CARREGAR_PERSONAGEM = 'Carregar personagem salvo'
-const SAIR_DO_JOGO  = 'Sair do jogo';
+const NOVO_PERSONAGEM = "Novo personagem";
+const CARREGAR_PERSONAGEM = "Carregar personagem salvo";
+const SAIR_DO_JOGO = "Sair do jogo";
 
 const startMenuOptions = () => {
   const questions = [
-      {
-          type: 'list',
-          name: 'selecaoCaminho',
-          message: 'Olá, selecione como prosseguir:',
-          choices: [NOVO_PERSONAGEM, CARREGAR_PERSONAGEM, SAIR_DO_JOGO],
-          default: NOVO_PERSONAGEM
-      }
-    ]
-    return inquirer.prompt(questions);
-}
+    {
+      type: "list",
+      name: "selecaoCaminho",
+      message: "Olá, selecione como prosseguir:",
+      choices: [NOVO_PERSONAGEM, CARREGAR_PERSONAGEM, SAIR_DO_JOGO],
+      default: NOVO_PERSONAGEM,
+    },
+  ];
+  return inquirer.prompt(questions);
+};
 
 const getOption = () => {
   inquirer.prompt(optionQuestion).then(async (answer) => {
@@ -89,14 +115,16 @@ const getOption = () => {
 };
 
 const handleInput = async (inp) => {
-  console.log(inp)
+  console.log(inp);
   if (inp == NOVO_PERSONAGEM) {
     const newPersonagem = await createNewCharacter();
     const personagemWithHabilidade = await addHabilidadeToPersonagem(
       newPersonagem
     );
     const personagemLocalizacao =
-      await PersonagensManager.getPersonagemLocalizacao(personagemWithHabilidade);
+      await PersonagensManager.getPersonagemLocalizacao(
+        personagemWithHabilidade
+      );
 
     await tellStoryGame();
     const location = await navigation(personagemLocalizacao);
--- game/src/user_interaction/praca_negociantes/praca_negociantes_control.js
+++ game/src/user_interaction/praca_negociantes/praca_negociantes_control.js
@@ -1,26 +1,29 @@
-const forms  = require('./praca_negociantes_forms');
+const forms = require("./praca_negociantes_forms");
 const ItemsManager = require("../../models/items/items_manager");
-const chalk = require('chalk');
-const Mochila = require('../../models/mochila');
+const chalk = require("chalk");
+const Mochila = require("../../models/mochila");
 
-async function negocianteFlow(personagem, negociante){
-    const itemsNegociante = await ItemsManager.getAllFromNegociante(negociante).then((res) => {return res});
-    console.log(chalk.red(`Você tem ${personagem.montante} moedas para usar.`));
-    const formItem = await forms.choiceFromItemsList(itemsNegociante);
-    itemsFlow(formItem, personagem, negociante);
-
+async function negocianteFlow(personagem, negociante) {
+  const itemsNegociante = await ItemsManager.getAllFromNegociante(negociante);
+  console.log(chalk.red(`Você tem ${personagem.montante} moedas para usar.`));
+  const formItem = await forms.choiceFromItemsList(itemsNegociante);
+  itemsFlow(formItem, personagem, negociante);
 }
 
-async function itemsFlow(formItem, personagem, negociante){
-    if(formItem.selecaoItem == forms.BACK_MENU){
-        return false;
-    } else {
-        const usoMochila = Mochila.usoMochila(await ItemsManager.getAllFromPersonagem(personagem));
-        await formItem.selecaoItem.vender(personagem, usoMochila);
-        await negocianteFlow(personagem, negociante);
-    }
+async function itemsFlow(formItem, personagem, negociante) {
+  console.log(formItem, personagem, negociante);
+
+  if (formItem.selecaoItem == forms.BACK_MENU) {
+    return false;
+  } else {
+    const usoMochila = Mochila.usoMochila(
+      await ItemsManager.getAllFromPersonagem(personagem)
+    );
+    await formItem.selecaoItem.vender(personagem, usoMochila);
+    await negocianteFlow(personagem, negociante);
+  }
 }
 
 module.exports = {
-    negocianteFlow: negocianteFlow
-}
+  negocianteFlow: negocianteFlow,
+};
